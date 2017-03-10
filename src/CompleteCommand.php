8dd2059d2d98decca58aab36e8832b62b5ba1008
Matt DaCosta <matthew.dacosta@hearst.co.uk>
2017-03-10 10:26:14 +0000

    add functionality to add and remove tasks

--- src/CompleteCommand.php
+++ src/CompleteCommand.php
@@ -5,16 +5,23 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ShowCommand extends Command {
+class CompleteCommand extends Command {
 
   public function configure()
   {
-    $this->setName('show')
-         ->setDescription('Show all tasks.');
+    $this->setName('complete')
+         ->setDescription('Complete a task by its id')
+         ->addArgument('id', InputArgument::REQUIRED);
   }
 
   public function execute(InputInterface $input, OutputInterface $output)
   {
+    $id = $input->getArgument('id');
+
+    $this->database->query('delete from tasks where id = :id', compact('id'));
+
+    $output->writeln('<info>Task Completed</info>');
+
     $this->showTasks($output);
   }
 
